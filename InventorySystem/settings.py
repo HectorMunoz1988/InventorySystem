b90759b396c785a1acda4d4d2131080a866ffaee
hector <hmh1988@hotmail.es>
2026-08-20 22:17:42 +0200

    Eliminar clave secreta del código

--- InventorySystem/settings.py
+++ InventorySystem/settings.py
@@ -21,10 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    'SECRET_KEY',
-    'django-insecure-46v!pmjbq0c44t$6dhcxl@oit9#w6!#bzm#r0qd8nsh+_#zq2w'
-)
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('RENDER') is None
 
